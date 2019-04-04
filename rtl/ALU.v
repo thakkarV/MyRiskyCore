16bb2bb2c130a25e4dd557c53e522845b7773ce4
thakkarv <thakkarv@bu.edu>
2019-04-04 16:45:47 -0400

    add non-tested full implementation of the RV32I ALU

--- rtl/ALU.v
+++ rtl/ALU.v
@@ -8,62 +8,85 @@ module ALU (
 );
 
 reg [31:0] output_reg;
-assign [2:0] funct3 = ALU_Control[2:0];
+wire [2:0] funct3;
+assign funct3 = ALU_Control[2:0];
 
 // ALU functions
 localparam [2:0]
-	FUNCT3_ADD = 3b'000,
-	FUNCT3_SLL = 3b'001,
-	FUNCT3_SLT = 3b'010,
-	FUNCT3_SLTU = 3b'011,
-	FUNCT3_XOR = 3b'100,
-	FUNCT3_SRL = 3b'101,
-	FUNCT3_OR = 3b'110,
-	FUNCT3_AND = 3b'111;
+    FUNCT3_ADD  = 3'b000,
+    FUNCT3_SHL  = 3'b001,
+    FUNCT3_SLT  = 3'b010,
+    FUNCT3_SLTU = 3'b011,
+    FUNCT3_XOR  = 3'b100,
+    FUNCT3_SHR  = 3'b101,
+    FUNCT3_OR   = 3'b110,
+    FUNCT3_AND  = 3'b111;
 
 // branch compare
 localparam [2:0]
-	BEQ  = 3b'000,
-	BNE  = 3b'001,
-	BLT  = 3b'100,
-	BGE  = 3b'101,
-	BLTU = 3b'110,
-	BGEU = 3b'111;
+    FUNCT3_BEQ  = 3'b000,
+    FUNCT3_BNE  = 3'b001,
+    FUNCT3_BLT  = 3'b100,
+    FUNCT3_BGE  = 3'b101,
+    FUNCT3_BLTU = 3'b110,
+    FUNCT3_BGEU = 3'b111;
+
+// comparators
+reg alu_eq, alu_lts, alu_ltu;
+always @* begin
+    alu_eq  = operand_A == operand_B;
+    alu_lts = $signed(operand_A) < $signed(operand_B);
+    alu_ltu = operand_A < operand_B;
+end
 
 always @* begin
-
-	case (ALU_Control[4:3]) begin
+	case (ALU_Control[4:3])
 		// add, and, or, xor, logical shifts
 		2'b00:
-			case (funct3) begin
-				FUNCT3_ADD: output_reg <= $signed(operand_A) + $signed(operand_B);
-				FUNCT3_SLL: output_reg <= operand_A << operand_B;
-				FUNCT3_SLT:
-					output_reg <= ($signed(operand_A) < $signed(operand_B)) ?
-						32'b1 : 32'b0;
-				FUNCT3_SLTU:
-					output_reg <= ($unsigned(operand_A) < $unsigned(operand_B)) ?
-						32'b1 : 32'b0;
-				FUNCT3_XOR: output_reg <= operand_A ^ operand_B;
-				FUNCT3_SRL: output_reg <= operand_A >> operand_B;
-				FUNCT3_OR:  output_reg <= operand_A | operand_B;
-				FUNCT3_AND: output_reg <= operand_A & operand_B;
+			case (funct3)
+				FUNCT3_ADD: output_reg = $signed(operand_A) + $signed(operand_B);
+                // for 2'b00 SHL is a logical shift right
+                FUNCT3_SHL: output_reg = operand_A << operand_B;
+				FUNCT3_SLT: output_reg = {31'b, alu_lts};
+				FUNCT3_SLTU: output_reg = {31'b, alu_ltu};
+				FUNCT3_XOR: output_reg = operand_A ^ operand_B;
+                // for 2'b00 SHR is a logical shift right
+				FUNCT3_SHR: output_reg = operand_A >> operand_B;
+				FUNCT3_OR:  output_reg = operand_A | operand_B;
+				FUNCT3_AND: output_reg = operand_A & operand_B;
 			endcase
 
 		// arithmetic shifts, sub
 		2'b01:
+            /* verilator lint_off CASEINCOMPLETE */
+            case (funct3)
+				FUNCT3_ADD: output_reg = $signed(operand_A) - $signed(operand_B);
+
+                // for 2'b00 SHL is an airthmetic shift right
+                FUNCT3_SHL: output_reg = $signed(operand_A) <<< $signed(operand_B);
+
+                // for 2'b00 SHR is an airthmetic shift right
+				FUNCT3_SHR: output_reg = $signed(operand_A) >>> $signed(operand_B);
+			endcase
 
 		// branch comparisions
 		2'b10:
-			case (funct3) begin
+            /* verilator lint_off CASEINCOMPLETE */
+			case (funct3)
+                FUNCT3_BEQ:  output_reg = {31'b0, alu_eq};
+                FUNCT3_BNE:  output_reg = {31'b0, !alu_eq};
+                FUNCT3_BLT:  output_reg = {31'b0, alu_lts};
+                FUNCT3_BGE:  output_reg = {31'b0, !alu_lts};
+                FUNCT3_BLTU: output_reg = {31'b0, alu_ltu};
+                FUNCT3_BGEU: output_reg = {31'b0, !alu_ltu};
 			endcase
 
-		// passthrough
+		// jal/jalr, passthrough op A
 		2'b11:
-			output_reg <= operand_A;
+			output_reg = operand_A;
 	endcase
 end
 
-assign ALU_result <= output_reg;
+assign ALU_result = output_reg;
 
 endmodule
