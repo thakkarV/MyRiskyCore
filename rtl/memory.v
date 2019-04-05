f068716292bad80203fc7450ddaac4cf024fdfda
thakkarv <thakkarv@bu.edu>
2019-04-04 21:46:38 -0400

    add initial implementation of RAM module

--- rtl/memory.v
+++ rtl/memory.v
@@ -18,8 +18,21 @@ module ram #(
 localparam RAM_DEPTH = 1 << ADDR_WIDTH;
 reg [DATA_WIDTH-1:0] ram [0:RAM_DEPTH-1];
 
-always @(posedge clock) begin
+// combinational read for the instruction fetch
+wire [ADDR_WIDTH-1:0] ri_address;
+assign ri_address = i_address >> 2; // word align the address
+assign i_read_data = ram[ri_address[ADDR_WIDTH-1:0]];
 
+// combinational read for the data
+wire [ADDR_WIDTH-1:0] rd_address;
+assign rd_address = d_address >> 2; // word align the address
+assign d_read_data = ram[rd_address[ADDR_WIDTH-1:0]]; // read the word
+
+// write on the pos-edge for the data
+always @(posedge clock) begin
+	if (wEn) begin
+		ram[rd_address[ADDR_WIDTH-1:0]] <= d_write_data;
+	end
 end
 
 endmodule
