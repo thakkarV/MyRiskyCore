ae7d9f31174f003cb5dfe164eb92a13a0fa88111
thakkarv <thakkarv@bu.edu>
2019-04-04 23:47:49 -0400

    add initial decode + immediate type decode + scaffold for full decoder
    new version of decoder that has most of the static decoing into register
    selects, immediate types + sign extensions and the required latches for
    dynamic decode. Also adds the skeleton code for the full dynamic
    decoder.

--- rtl/decode.v
+++ rtl/decode.v
@@ -3,7 +3,7 @@ module decode #(
 ) (
 	// Inputs from Fetch
 	input [ADDRESS_BITS-1:0] PC,
-	input [31:0] instruction,
+	input [31:0] instr,
 
 	// Inputs from Execute/ALU
 	input [ADDRESS_BITS-1:0] JALR_target,
@@ -45,10 +45,10 @@ localparam [6:0]
 	AUIPC   = 7'b0010111,
 	LUI     = 7'b0110111;
 
+localparam [2:0] ZERO_3 = 3'b000;
+
 // These are internal wires that I used. You can use them but you do not have to.
 // Wires you do not use can be deleted.
-
-
 wire[31:0] sb_imm_32;
 wire[31:0] u_imm_32;
 wire[31:0] i_imm_32;
@@ -59,107 +59,114 @@ wire [1:0] extend_sel;
 wire [ADDRESS_BITS-1:0] branch_target;
 wire [ADDRESS_BITS-1:0] jal_target;
 
-localparam [6:0] R_TYPE = 7'b0110011;
-localparam [6:0] I_TYPE = 7'b0010011;
-localparam [6:0] S_TYPE = 7'b0100011;
-localparam [2:0] ZERO_3 = 3'b000;
+// static decoding into different fields of all instruction types
+// major and minor opcodes
+wire [6:0] opcode = instr[6:0];
+wire [6:0] funct7 = instr[31:25];
+wire [2:0] funct3 = instr[14:12];
 
-wire [6:0] opcode;
-wire [6:0] funct7;
-wire [2:0] funct3;
+// register selects
+assign read_sel1 = instr[19:15];
+assign read_sel2 = instr[24:20];
+assign write_sel = instr[11:7];
 
-wire [11:0] imm12;
-wire [11:0] i_imm; // Immidiate for I-Type instructions
-wire [11:0] s_imm; // Immidiate for S-Type instructions
-
-assign opcode = instruction[6:0];
-assign funct7 = instruction[31:25];
-assign funct3 = instruction[14:12];
-
-// Immediate
-assign i_imm = instruction[31:20];
-assign s_imm = {instruction[31:25], instruction[4:0]};
-wire[6:0]  s_imm_msb;
-wire[4:0]  s_imm_lsb;
-wire[19:0] u_imm;
-wire[11:0] i_imm_orig;
-wire[19:0] uj_imm;
-wire[11:0] s_imm_orig;
-wire[12:0] sb_imm_orig;
-reg [11:0] imm12_latch;
-
-// static assignments for data buses
-assign read_sel1 = instruction[19:15];
-assign read_sel2 = instruction[24:20];
-assign write_sel = instruction[11:7];
+// all immediate types; sign extension always uses instr[31]
+wire [31:0] i_imm32 = {{20{instr[31]}}, instr[31:20]};
+wire [31:0] s_imm32 = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+wire [31:0] b_imm32 = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+wire [31:0] u_imm32 = {instr[31:12], 12'b0};
+wire [31:0] j_imm32 = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+reg  [31:0] imm32_reg;
 
 // combinational logic for control buses
-reg wb_sel_latch;
-reg op_b_sel_latch;
-reg [5:0] alu_control_latch;
-reg reg_wEn_latch;
-reg mem_wEn_latch;
-
+reg wb_sel_reg;
+reg op_b_sel_reg;
+reg [5:0] alu_control_reg;
+reg reg_wEn_reg;
+reg mem_wEn_reg;
 always @* begin
-	// ALU compute instruction with 2 RS, 1 RD
+	// ALU compute instr with 2 RS, 1 RD
 	if (opcode == R_TYPE) begin
-		wEn <= 1;
-		mem_wEn <= 0;
-		op_b_sel_latch <= 1;
-		wb_sel_latch <= 0;
-		imm12_latch <= i_imm;
+		wEn = 1;
+		mem_wEn = 0;
+		op_b_sel_reg = 1;
+		wb_sel_reg = 0;
 
 		// sub or other?
-		if (funct7 == 7'b0100000) alu_control_latch <= 6'b_010_000;
-		else alu_control_latch <={ZERO_3, funct3};
+		if (funct7 == 7'b0100000) alu_control_reg = 6'b_010_000;
+		else alu_control_reg = {ZERO_3, funct3};
 	end
 
 	// ALU compute with 1 RS and immediate
 	else if (opcode == I_TYPE) begin
-		wEn <= 1;
-		mem_wEn <= 0;
-		op_b_sel_latch <= 0;
-		alu_control_latch <= {ZERO_3, funct3};
-		wb_sel_latch <= 0;
-		imm12_latch <= i_imm;
+		wEn = 1;
+		mem_wEn = 0;
+		op_b_sel_reg = 0;
+		alu_control_reg = {ZERO_3, funct3};
+		wb_sel_reg = 0;
+		imm32_reg = i_imm32;
 	end
 
 	// load word
-	else if (opcode == 7'b0000011) begin
-		wEn <= 1;
-		mem_wEn <= 0;
-		op_b_sel_latch <= 0;
+	else if (opcode == LOAD) begin
+		wEn = 1;
+		mem_wEn = 0;
+		op_b_sel_reg = 0;
 		// for load/store, ALU acts as AGU
-		alu_control_latch <= {ZERO_3, funct3};
-		wb_sel_latch <= 1;
-		imm12_latch <= i_imm;
+		alu_control_reg = {ZERO_3, funct3};
+		wb_sel_reg = 1;
+		imm32_reg = i_imm32;
 	end
 
 	// store word
-	else if (opcode == S_TYPE) begin
-		wEn <= 0;
-		mem_wEn <= 1;
-		op_b_sel_latch <= 0;
+	else if (opcode == STORE) begin
+		wEn = 0;
+		mem_wEn = 1;
+		op_b_sel_reg = 0;
 		// for load/store, ALU acts as AGU
-		alu_control_latch <= {ZERO_3, funct3};
-		wb_sel_latch <= 0;
-		imm12_latch <= s_imm;
+		alu_control_reg = {ZERO_3, funct3};
+		wb_sel_reg = 0;
+		imm32_reg = s_imm32;
 	end
+
+    // branch instructions
+    else if (opcode == BRANCH) begin
+
+    end
+
+    // jal
+    else if (opcode == JAL) begin
+
+    end
+
+    // jalr
+    else if (opcode == JALR) begin
+
+    end
+
+    // auipc
+    else if (opcode == AUIPC) begin
+
+    end
+
+    // lui
+    else if (opcode == LUI) begin
+
+    end
 
 	// otherwise switch off all control signals
 	else begin
-		wEn <= 0;
-		mem_wEn <= 0;
-		op_b_sel_latch <= 0;
-		wb_sel_latch <= 0;
+		wEn = 0;
+		mem_wEn = 0;
+		op_b_sel_reg = 0;
+		wb_sel_reg = 0;
 	end
 end
 
-assign ALU_Control = alu_control_latch;
-assign wb_sel = wb_sel_latch;
-assign op_B_sel = op_b_sel_latch;
-assign wEn = reg_wEn_latch;
-assign mem_wEn = mem_wEn_latch;
-assign imm12 = imm12_latch;
-assign imm32 = { {20{imm12[11]}}, imm12 };
+assign ALU_Control = alu_control_reg;
+assign wb_sel = wb_sel_reg;
+assign op_B_sel = op_b_sel_reg;
+assign wEn = reg_wEn_reg;
+assign mem_wEn = mem_wEn_reg;
+assign imm32 = imm32_reg;
 endmodule
