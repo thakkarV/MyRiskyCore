9e53be548ab1b80176e510c9d07b2582e5f4c37e
thakkarv <thakkarv@bu.edu>
2019-04-15 23:21:29 -0400

    fix messy address indexing for ram.v

--- rtl/ram.v
+++ rtl/ram.v
@@ -16,23 +16,17 @@ module ram #(
 );
 
 localparam RAM_DEPTH = 1 << ADDR_WIDTH;
-reg [DATA_WIDTH-1:0] ram [0:RAM_DEPTH-1];
+reg [DATA_WIDTH-1:0] sram [0:RAM_DEPTH-1];
 
-// combinational read for the instruction fetch
-wire [ADDR_WIDTH-1:0] ri_address;
-assign ri_address = i_address >> 2; // word align the address
-assign i_read_data = ram[ri_address[ADDR_WIDTH-1:0]];
+// combinational read for instruction fetch
+assign d_read_data = sram[d_address];
 
-// combinational read for the data
-wire [ADDR_WIDTH-1:0] rd_address;
-assign rd_address = d_address >> 2; // word align the address
-assign d_read_data = ram[rd_address[ADDR_WIDTH-1:0]]; // read the word
+// combinational read for data
+assign i_read_data = sram[i_address];
 
 // write on the pos-edge for the data
 always @(posedge clock) begin
-	if (wEn) begin
-		ram[rd_address[ADDR_WIDTH-1:0]] <= d_write_data;
-	end
+	if (wEn) sram[d_address] <= d_write_data;
 end
 
 endmodule
