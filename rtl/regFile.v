09fa1a275cc9179cf751df0ff727bec9922de144
thakkarv <thakkarv@bu.edu>
2019-04-17 19:48:47 -0400

    fix doubly driven x0 register in regfile

--- rtl/regFile.v
+++ rtl/regFile.v
@@ -18,15 +18,7 @@ assign read_data2 = reg_file[read_sel2];
 
 // writes on the positive edge
 always @(posedge clock) begin
-	if (wEn == 1'b1) begin
-		// make sure we do not write to zero register (x0)
-		if (write_sel == 5'b0) reg_file[0] = 32'b0;
-		else reg_file[write_sel] = write_data;
-	end
-end
-
-always @* begin
-	if (reset) begin
+    if (reset) begin
 		reg_file[00] = 0;
 		reg_file[01] = 0;
 		reg_file[02] = 0;
@@ -59,6 +51,10 @@ always @* begin
 		reg_file[29] = 0;
 		reg_file[30] = 0;
 		reg_file[31] = 0;
+	end
+	else if (wEn == 1'b1) begin
+		// make sure we do not write to zero register (x0)
+		if (write_sel != 5'b0) reg_file[write_sel] = write_data;
 	end
 end
 
