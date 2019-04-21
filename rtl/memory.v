e6ab33a58492c657495c3911bee1180509a30d79
thakkarv <thakkarv@bu.edu>
2019-04-21 18:16:41 -0400

    fix and add more tests for memory.v

--- rtl/memory.v
+++ rtl/memory.v
@@ -16,19 +16,19 @@ module ram #(
 );
 
 localparam RAM_DEPTH = 1 << ADDR_WIDTH;
-reg [DATA_WIDTH-1:0] memory [0:RAM_DEPTH-1];
+reg [DATA_WIDTH-1:0] ram [0:RAM_DEPTH-1];
 
 // combinational read for instruction fetch
-assign word_aligned_d_addr = d_address >> 2;
-assign d_read_data = sram[word_aligned_d_addr];
+wire [ADDR_WIDTH-1:0] word_aligned_d_addr = d_address >> 2;
+assign d_read_data = ram[word_aligned_d_addr];
 
 // combinational read for data
-assign word_aligned_i_addr = i_address >> 2;
-assign i_read_data = sram[word_aligned_i_addr];
+wire [ADDR_WIDTH-1:0] word_aligned_i_addr = i_address >> 2;
+assign i_read_data = ram[word_aligned_i_addr];
 
 // write on the pos-edge for the data
 always @(posedge clock) begin
-	if (wEn) sram[d_address] <= d_write_data;
+	if (wEn) ram[d_address] <= d_write_data;
 end
 
 endmodule
--- test/memory_tb.v
+++ test/memory_tb.v
@@ -2,66 +2,85 @@ module memory_tb();
 
 reg clock;
 reg wEn;
-reg [15:0] address;
-reg [31:0] write_data;
-wire [31:0] read_data;
+reg [15:0] i_address;
+wire [31:0] i_read_data;
 
-memory uut (
-  .clock(clock),
-  .wEn(wEn),
-  .address(address),
-  .write_data(write_data),
-  .read_data(read_data)
+reg [15:0] d_address;
+wire [31:0] d_read_data;
+reg [31:0] d_write_data;
+
+ram #(
+	.ADDR_WIDTH(16)
+) uut (
+	.clock(clock),
+    .i_address(i_address),
+    .i_read_data(i_read_data),
+	.wEn(wEn),
+    .d_address(d_address),
+    .d_read_data(d_read_data),
+    .d_write_data(d_write_data)
 );
 
 always #5 clock = ~clock;
 
 initial begin
-  clock = 1'b1;
-  write_data = 0;
-  address = 16'd0;
-  write_data = 16'd0;
-  wEn = 1'b0;
+	clock = 1;
+	i_address = 32'h134;
+	d_write_data = 0;
+	d_address = 0;
+	wEn = 1'b0;
 
-//  $readmemh("/home/void/Desktop/ClassStuff/MyRiskyCore/test/fibonacci.vmh", uut.main_memory.sram); // Should put 0x00000015 in register x9
-  $readmemh("/home/void/Desktop/ClassStuff/MyRiskyCore/test/gcd.vmh", uut.main_memory.sram); // Should put 0x00000010 in register x9
+	//  $readmemh("/home/void/Desktop/ClassStuff/MyRiskyCore/test/fibonacci.vmh", uut.memory);
+	$readmemh("/home/void/Desktop/ClassStuff/MyRiskyCore/test/gcd.vmh", uut.ram);
 
-  // read loaded instruction stream
-  #10
-  address = 0;
-  $display("Address %d: %h", address, read_data);
-  #10
-  address = 1;
-  $display("Address %d: %h", address, read_data);
-  #10
-  address = 2;
-  $display("Address %d: %h", address, read_data);
-  #10
-  address = 3;
-  $display("Address %d: %h", address, read_data);
-  #10
-  address = 4;
-  $display("Address %d: %h", address, read_data);
-  #10
-  address = 5;
-  $display("Address %d: %h", address, read_data);
-  #10
-  address = 6;
-  $display("Address %d: %h", address, read_data);
-  #10
-  address = 7;
-  $display("Address %d: %h", address, read_data);
-  #10
-  write_data = 1;
-  address = 4;
-  #10
-  $display("Address %d: %h", address, read_data);
-  write_data = 2;
-  address = 8;
-  #10
-  $display("Address %d: %h", address, read_data);
-  #10
-  $stop();
+	// read loaded instruction stream
+	#10
+	d_address = 0;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_address = 1;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_address = 2;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_address = 3;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_address = 4;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_address = 5;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_address = 6;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_address = 7;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+    d_address = 8;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+    d_address = 12;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+    d_address = 16;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+    d_address = 20;
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	d_write_data = 32'hDEADBEEF;
+	d_address = 4;
+	#10
+	$display("d_address %d: %h", d_address, d_read_data);
+	d_write_data = -1;
+	d_address = 8;
+	#10
+	$display("d_address %d: %h", d_address, d_read_data);
+	#10
+	$stop();
 
 end
 
