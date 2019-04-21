1628369848d0867f50f18cc03d82968194d8b658
thakkarv <thakkarv@bu.edu>
2019-04-21 10:44:48 -0400

    add more test cases for memory

--- test/memory_tb.v
+++ test/memory_tb.v
@@ -24,11 +24,53 @@ initial begin
   write_data = 16'd0;
   wEn = 1'b0;
 
-  #1
+  always #5 clock = ~clock;
+
+task print_state;
+  begin
+    $display("Time:\t%0d", $time);
+    for( x=0; x<32; x=x+1) begin
+      $display("Register %d: %h", x, dut.regFile_inst.reg_file[x]);
+    end
+    $display("--------------------------------------------------------------------------------");
+    $display("\n\n");
+  end
+endtask
+
+initial begin
+  clock = 1'b1;
+  reset = 1'b1;
+  write_data = 0;
+  // Make sure the .vmh file is in the same directory that you launched the
+  // simulation from.
+  $readmemh("./fibonacci.vmh", dut.main_memory.ram);
+
+  // read loaded instruction stream
   #10
-  wEn = 1'b1;
-  #10
+  address = 0;
   $display("Address %d: %h", address, read_data);
+  #10
+  address = 1;
+  $display("Address %d: %h", address, read_data);
+  #10
+  address = 2;
+  $display("Address %d: %h", address, read_data);
+  #10
+  address = 3;
+  $display("Address %d: %h", address, read_data);
+  #10
+  address = 4;
+  $display("Address %d: %h", address, read_data);
+  #10
+  address = 5;
+  $display("Address %d: %h", address, read_data);
+  #10
+  address = 6;
+  $display("Address %d: %h", address, read_data);
+  #10
+  address = 7;
+  $display("Address %d: %h", address, read_data);
+  #10
   write_data = 1;
   address = 4;
   #10
@@ -37,15 +79,7 @@ initial begin
   address = 8;
   #10
   $display("Address %d: %h", address, read_data);
-
-
-
-  /***************************
-  * Add more test cases here *
-  ***************************/
-
-
-  #100
+  #10
   $stop();
 
 end
