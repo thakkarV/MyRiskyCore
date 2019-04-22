6def3a0aa835b0d7b53b70316ae63ff69b2ed892
thakkarv <thakkarv@bu.edu>
2019-04-22 09:37:29 -0400

    fix memory.v for word aligned data write address

--- rtl/memory.v
+++ rtl/memory.v
@@ -19,16 +19,16 @@ localparam RAM_DEPTH = 1 << ADDR_WIDTH;
 reg [DATA_WIDTH-1:0] ram [0:RAM_DEPTH-1];
 
 // combinational read for instruction fetch
-wire [ADDR_WIDTH-1:0] word_aligned_d_addr = d_address >> 2;
-assign d_read_data = ram[word_aligned_d_addr];
-
-// combinational read for data
 wire [ADDR_WIDTH-1:0] word_aligned_i_addr = i_address >> 2;
 assign i_read_data = ram[word_aligned_i_addr];
 
+// combinational read for data
+wire [ADDR_WIDTH-1:0] word_aligned_d_addr = d_address >> 2;
+assign d_read_data = ram[word_aligned_d_addr];
+
 // write on the pos-edge for the data
 always @(posedge clock) begin
-	if (wEn) ram[d_address] <= d_write_data;
+	if (wEn) ram[word_aligned_d_addr] <= d_write_data;
 end
 
 endmodule
