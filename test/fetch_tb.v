6c8c987659ea2b8d8fe04a4c511c85d7ea28b971
thakkarv <thakkarv@bu.edu>
2019-04-22 20:15:43 -0400

    add initial test bench for fetch.v

--- test/fetch_tb.v
+++ test/fetch_tb.v
@@ -9,39 +9,52 @@ reg [ADDRESS_BITS-1:0] target_PC;
 wire [ADDRESS_BITS-1:0] PC;
 
 fetch #(
-  .ADDRESS_BITS(ADDRESS_BITS)
+	.ADDRESS_BITS(ADDRESS_BITS)
 ) uut (
-  .clock(clock),
-  .reset(reset),
-  .next_PC_select(next_PC_select),
-  .target_PC(target_PC),
-  .PC(PC)
+	.clock(clock),
+	.reset(reset),
+	.next_PC_select(next_PC_select),
+	.target_PC(target_PC),
+	.PC(PC)
 );
 
 always #5 clock = ~clock;
 
 
 initial begin
-  clock = 1'b1;
-  reset = 1'b1;
-  next_PC_select = 1'b0;
-  target_PC = 16'h0000;
+	clock = 1'b1;
+	reset = 1'b1;
+	next_PC_select = 1'b0;
+	target_PC = 16'h0000;
 
-  #1
-  #10
-  reset = 1'b0;
+	#1
+	#10
+	reset = 1'b0;
 
-  #10
-  $display("PC: %h", PC);
+	#10
+	$display("PC: %h", PC);
 
-  /***************************
-  * Add more test cases here *
-  ***************************/
+	next_PC_select = 1;
+	target_PC = 16'h0000;
+	#10
 
+	next_PC_select = 0;
+	#10
+	#10
+	#10
+	#10
+	#10
+	next_PC_select = 1;
+	target_PC = 16'h0100;
+	#10
+	#10
+	next_PC_select = 0;
+	#10
+	#10
+	#10
+	#10
 
-  #100
-  $stop();
-
+	$stop();
 end
 
 endmodule
